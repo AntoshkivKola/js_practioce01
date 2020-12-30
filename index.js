2783eb450f43c95ff449d612f49e0db49b911705
Kola Antoshkiv <ak.pkqtor@gmail.com>
2020-12-30 20:16:56 +0200

    Add 7 task slice

--- index.js
+++ index.js
@@ -20,12 +20,17 @@ console.log(arr1);
 
 //5.  Дан массив ['js', 'css', 'jq']. Выведите на экран первый элемент и удалите его.
 let arr4 = ['js', 'css', 'jq'];
-console.log(arr4)
+console.log(arr4);
 console.log(arr4.shift());
-console.log(arr4)
+console.log(arr4);
 
 //6. Дан массив ['js', 'css', 'jq']. Выведите на экран последний элемент.
 arr4 = ['js', 'css', 'jq'];
-console.log(arr4)
+console.log(arr4);
 console.log(arr4.pop());
-console.log(arr4)
+console.log(arr4);
+
+//7. Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый элементы [1, 2, 3].
+const arr5 = [1, 2, 3, 4, 5];
+let arr6 = arr5.slice(0,3);
+console.log(arr6);
