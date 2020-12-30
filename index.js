fefd6e8968c8ba2f83a54e415a89bf948b034c68
Kola Antoshkiv <ak.pkqtor@gmail.com>
2020-12-30 20:29:20 +0200

    Add 9-12 task`s splice method

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ console.log(arr1);
 arr1.reverse();
 arr1.push(4, 5, 6);
 console.log(arr1);
-
 //4. Дан массив [1, 2, 3]. Добавьте ему в начало элементы 4, 5, 6.
 arr1.unshift(4,5,6);
 console.log(arr1);
@@ -23,7 +22,6 @@ let arr4 = ['js', 'css', 'jq'];
 console.log(arr4);
 console.log(arr4.shift());
 console.log(arr4);
-
 //6. Дан массив ['js', 'css', 'jq']. Выведите на экран последний элемент.
 arr4 = ['js', 'css', 'jq'];
 console.log(arr4);
@@ -36,4 +34,23 @@ let arr6 = arr5.slice(0,3);
 console.log(arr6);
 //8. Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый элементы [4, 5].
 arr6 = arr5.slice(3);
+console.log(arr6);
+
+//9. Дан массив [1, 2, 3, 4, 5]. С помощью метода splice преобразуйте массив в [1, 4, 5].
+arr6 = [1, 2, 3, 4, 5];
+arr6.splice(1,2);
+console.log(arr6);
+//10. Дан массив [1, 2, 3, 4, 5]. С помощью метода splice запишите в новый массив элементы [2, 3, 4].
+arr6 = [1, 2, 3, 4, 5];
+arr7 = arr6.splice(1,3);
+console.log(arr7);
+//11. Дан массив [1, 2, 3, 4, 5]. С помощью метода splice сделайте из него массив [1, 2, 3, 'a', 'b', 'c', 4, 5].
+arr6 = [1, 2, 3, 4, 5];
+arr6.splice(3, 0, 'a', 'b', 'c');
+console.log(arr6);
+//12. Дан массив [1, 2, 3, 4, 5]. С помощью метода splice сделайте из него массив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].
+arr6 = [1, 2, 3, 4, 5];
+arr6.splice(1, 0, 'a', 'b');
+arr6.splice(6, 0, 'c');
+arr6.splice(8, 0, 'e');
 console.log(arr6);
