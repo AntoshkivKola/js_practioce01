2ce5b5f2d5e5fc4bb82526fd350aa15102b54f94
Kola Antoshkiv <ak.pkqtor@gmail.com>
2020-12-30 19:42:19 +0200

    Add average function

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const mas = [4, -2, 5, 19, -130, 0, 10];
  * @param {Array numbers} mas
  * @returns {number}
  */
-function getMax(mas){
+function getMax(mas) {
   let max = 0;
- for(let i = 0;i <= mas.length ;i++){
-   if(mas[i] > max){
-    max = mas[i];
-   }
- }
- return max;
+  for (let i = 0; i < mas.length; i++) {
+    if (mas[i] > max) {
+      max = mas[i];
+    }
+  }
+  return max;
 }
 
 /**
@@ -26,15 +26,37 @@ function getMax(mas){
  * @param {Array numbers} mas
  * @returns {number}
  */
-function getMin(mas){
+function getMin(mas) {
   let min = 0;
- for(let i = 0; i <= mas.length ;i++){
-   if(mas[i] < min){
-    min = mas[i];
-   }
- }
- return min;
+  for (let i = 0; i < mas.length; i++) {
+    if (mas[i] < min) {
+      min = mas[i];
+    }
+  }
+  return min;
 }
 
 console.log(getMax(mas));
 console.log(getMin(mas));
+
+
+// task2 
+/*
+Дан массив arr. \
+Найдите среднее арифметическое его элементов. 
+Проверьте задачу на массиве с элементами 12, 15, 20, 25, 59, 79.
+*/
+const arr = [12, 15, 20, 25, 59, 79];
+/**
+ * get average of numbers
+ * @param {Array numbers} mas
+ * @returns {number}
+ */
+function getAverage(mas) {
+  let average = 0;
+  for (let i = 0; i < mas.length; i++) {
+    average += mas[i];
+  }
+  return average / mas.length;
+}
+console.log(getAverage(arr));
