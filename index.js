a03fb002aa6d4e5595dd37572cac4727753c373a
Kola Antoshkiv <ak.pkqtor@gmail.com>
2020-12-30 19:47:32 +0200

    Add  array of random numbres

--- index.js
+++ index.js
@@ -69,3 +69,9 @@ console.log(getAverage(arr));
 */
 
 console.log(Math.round(Math.random()*100));
+
+let arr1 = [];
+for(let i = 0; i < 10; i++){
+  arr1.push(Math.round(Math.random()*100));
+}
+console.log(arr1);
